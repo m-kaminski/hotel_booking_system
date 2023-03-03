1ceaea4f9b76978332d68abfcfef95fcf1f15ac4
Matt (Maciej) Kaminski <maciej.a.kaminski@gmail.com>
2023-03-02 22:11:32 -0600

    Startup script can be used to run only backend or only frontend or both (it used to run both every single time)

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
+# parameters: BE: backend only, FE: frontend only
 
 source ./run_options.sh
 
+if [ -e $1 ] 
+then 
+    SCOPE="BEFE"
+else
+    SCOPE=$1
+fi
 
 
 cd ..
 
-echo "LOADING STATIC CONTENT TO: " $STATIC_CONTENT_PATH
+if [ $SCOPE == BE  ] 
+then
+    echo "Skipping frontend"
+else
+    echo "LOADING STATIC CONTENT TO: " $STATIC_CONTENT_PATH
 
+    echo "SYNCING STATIC CONTENT"
+    sudo mkdir -p $STATIC_CONTENT_PATH
+    sudo setfacl -m $NGINX_USER:rwx -R $STATIC_CONTENT_PATH
+    sudo rsync -r frontends/ $STATIC_CONTENT_PATH
+    sudo chown -R nginx $STATIC_CONTENT_PATH
 
-echo "SYNCING STATIC CONTENT"
-sudo mkdir -p $STATIC_CONTENT_PATH
-sudo setfacl -m $NGINX_USER:rwx -R $STATIC_CONTENT_PATH
-sudo rsync -r frontends/ $STATIC_CONTENT_PATH
-sudo chown -R nginx $STATIC_CONTENT_PATH
-cd backends/booking/fcgi-cpp
-make
+    echo "Webpage will be accessible at " $STATIC_CONTENT_URL
+
+fi
 
 #echo "RESTARTING nginx"
 #service nginx restart
 
-echo "RUNNING fcgi-main"
-echo "Webpage will be accessible at " $STATIC_CONTENT_URL
-spawn-fcgi -p $FCGI_PORT -n fcgi-main
+if [ $SCOPE = FE ]
+then
+    echo "Skipping backend"
+else
+    cd backends/booking/fcgi-cpp
+    make
 
+
+    echo "RUNNING fcgi-main"
+    spawn-fcgi -p $FCGI_PORT -n fcgi-main
+fi
