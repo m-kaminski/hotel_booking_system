e1083fca64e8112fb3762e2182289f4e45d12572
Matt (Maciej) Kaminski <maciej.a.kaminski@gmail.com>
2023-03-04 04:10:47 -0600

    Fixed SQL syntax

--- notes/schema.sql
+++ notes/schema.sql
@@ -5,67 +5,67 @@
 CREATE EXTENSION pgcrypto;
 
 CREATE TABLE IF NOT EXISTS "hotel" (
-	"id"                SERIAL,
-	"name"              TEXT,
-	"address"           TEXT,
-	"image"				VARCHAR(1024), -- for header of hotel website
-	PRIMARY KEY( id )
+    "id"                SERIAL,
+    "name"              TEXT,
+    "address"           TEXT,
+    "image"             VARCHAR(1024), -- for header of hotel website
+    PRIMARY KEY( id )
 );
 
-CREATE TABLE IF NOT EXISTS "hotel_image" {
-	"id"				SERIAL,
-	"url"				VARCHAR(1024),
-	"hotel_fk"          INTEGER REFERENCES "hotel",
-	"order"				INTEGER,
-	PRIMARY KEY( id )
-}
+CREATE TABLE IF NOT EXISTS "hotel_image" (
+    "id"                SERIAL,
+    "url"               VARCHAR(1024),
+    "hotel_fk"          INTEGER REFERENCES "hotel",
+    "order"             INTEGER,
+    PRIMARY KEY( id )
+);
 
 -- if a hotel consists of more than one buildings, it's a many-1 relationship -- 
 CREATE TABLE IF NOT EXISTS "building" (
-	"id"                SERIAL,
-	"identifier"        text, -- descriptive text --
-	"name"              text,
-	"address"           text,
-	"hotel_fk"          INTEGER REFERENCES "hotel",
-	PRIMARY KEY( id )
+    "id"                SERIAL,
+    "identifier"        text, -- descriptive text --
+    "name"              text,
+    "address"           text,
+    "hotel_fk"          INTEGER REFERENCES "hotel",
+    PRIMARY KEY( id )
 );
 
-CREATE TABLE IF NOT EXISTS "building_image" {
-	"id"				SERIAL,
-	"url"				VARCHAR(1024),
-	"building_fk"       INTEGER REFERENCES "building",
-	"order"				INTEGER,
-	PRIMARY KEY( id )
-}
+CREATE TABLE IF NOT EXISTS "building_image" (
+    "id"                SERIAL,
+    "url"               VARCHAR(1024),
+    "building_fk"       INTEGER REFERENCES "building",
+    "order"             INTEGER,
+    PRIMARY KEY( id )
+);
 
 -- a room type --
 CREATE TABLE IF NOT EXISTS "room_type" (
-	"id"                SERIAL,
-	"sqft"              INTEGER,
+    "id"                SERIAL,
+    "sqft"              INTEGER,
     "name"              TEXT,
-	"description"       TEXT, -- descriptive text (highlight facing, amenities etc)
+    "description"       TEXT, -- descriptive text (highlight facing, amenities etc)
     "smoking"           BOOLEAN,
     "beds"              SMALLINT,
     "disability"        BOOLEAN,
-	"hotel_fk"          INTEGER REFERENCES "hotel",
-	PRIMARY KEY( id )
+    "hotel_fk"          INTEGER REFERENCES "hotel",
+    PRIMARY KEY( id )
 );
 
-CREATE TABLE IF NOT EXISTS "room_image" {
-	"id"				SERIAL,
-	"url"				VARCHAR(1024),
-	"room_type_fk"      INTEGER REFERENCES "room_type",
-	"order"				INTEGER,
-	PRIMARY KEY( id )
-}
+CREATE TABLE IF NOT EXISTS "room_image" (
+    "id"                SERIAL,
+    "url"               VARCHAR(1024),
+    "room_type_fk"      INTEGER REFERENCES "room_type",
+    "order"             INTEGER,
+    PRIMARY KEY( id )
+);
 
 CREATE TABLE IF NOT EXISTS "room" (
-	"id"                SERIAL,
-	"room_number"       VARCHAR(10),
-	"floor"             INTEGER,
-	"building_fk"       INTEGER REFERENCES "building", -- building room is in
-	"room_type_fk"      INTEGER REFERENCES "room_type",
-	PRIMARY KEY( id )
+    "id"                SERIAL,
+    "room_number"       VARCHAR(10),
+    "floor"             INTEGER,
+    "building_fk"       INTEGER REFERENCES "building", -- building room is in
+    "room_type_fk"      INTEGER REFERENCES "room_type",
+    PRIMARY KEY( id )
 );
 
 CREATE TABLE IF NOT EXISTS "person" (
@@ -75,23 +75,23 @@ CREATE TABLE IF NOT EXISTS "person" (
     "legal_last_name"   VARCHAR(64),
     "preferred_name"    VARCHAR(64),
     "email"             VARCHAR(64) NOT NULL UNIQUE,
-	"phone_num"			 BIGINT,
-	"phone_country_code" SMALLINT,
-	"password"			VARCHAR(70),
-	PRIMARY KEY( id )
+    "phone_num"         BIGINT,
+    "phone_country_code" SMALLINT,
+    "password"          VARCHAR(70),
+    PRIMARY KEY( id )
 );
 
 CREATE TABLE IF NOT EXISTS "booking" (
-	"id"                SERIAL,
-	"checkin"           TIMESTAMP,
-	"checkout"          TIMESTAMP,
-	"special_requests"	TEXT,
-	"adults"			SMALLINT,
-	"children"			SMALLINT,
-	"hotel_fk"          INTEGER REFERENCES "hotel",
-	"room_fk"           INTEGER REFERENCES "room", -- bookings refer to room type, unless checked in
-	"room_type_fk"      INTEGER REFERENCES "room_type",
-	"guest_fk"          INTEGER REFERENCES "guest",
-	PRIMARY KEY( id )
+    "id"                SERIAL,
+    "checkin"           TIMESTAMP,
+    "checkout"          TIMESTAMP,
+    "special_requests"  TEXT,
+    "adults"            SMALLINT,
+    "children"          SMALLINT,
+    "hotel_fk"          INTEGER REFERENCES "hotel",
+    "room_fk"           INTEGER REFERENCES "room", -- bookings refer to room type, unless checked in
+    "room_type_fk"      INTEGER REFERENCES "room_type",
+    "guest_fk"          INTEGER REFERENCES "guest",
+    PRIMARY KEY( id )
 );
 
