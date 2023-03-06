c70b3de327facd1432da6e19346fc363c2beacb7
Matt (Maciej) Kaminski <maciej.a.kaminski@gmail.com>
2023-03-05 21:25:42 -0600

    Added persistence handling for rooms

--- backends/booking/service/src/main/java/com/hbrs/booking/service/persistence/Rooms.java
+++ backends/booking/service/src/main/java/com/hbrs/booking/service/persistence/Rooms.java
@@ -10,13 +10,65 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.jdbc.core.JdbcTemplate;
  
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Rooms {
+    private Connection conn;
+
+    public Rooms() {
+
+        // auto close connection
+        try  {
+            conn = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:5432/hbrs", "LOGIN", "PASSWORD");
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public List<RoomType> getRooms() {
-
+        String SQL_SELECT = "SELECT T.*, R.count FROM" +
+                "(SELECT room_type_fk, COUNT(*) AS count FROM room GROUP BY room_type_fk) as R "+
+                 "JOIN room_type T ON R.room_type_fk = T.id";
 		List<RoomType> response = new ArrayList<>();
-		response.add(new RoomType(1, 400, "Room 1", "foo", true, 1, true));
-		response.add(new RoomType(1, 500, "Room 2", "foo", false, 2, true));
+
+        try (PreparedStatement preparedStatement = conn.prepareStatement(SQL_SELECT)) {
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+
+        while (resultSet.next()) {
+
+            long id = resultSet.getLong("id");
+            int sqft = resultSet.getInt("sqft");
+            String name = resultSet.getString("name");
+            String description = resultSet.getString("description");
+            boolean smoking = resultSet.getBoolean("smoking");
+            int beds = resultSet.getInt("beds");
+            boolean disability = resultSet.getBoolean("disability");
+            response.add(new RoomType(id, sqft, name, description, smoking, beds, disability));
+
+
+        }
+
+    } catch (SQLException e) {
+        System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+		//response.add(new RoomType(1, 400, "Room 1", "foo", true, 1, true));
+		//response.add(new RoomType(1, 500, "Room 2", "foo", false, 2, true));
         return response;        
     }
 }
